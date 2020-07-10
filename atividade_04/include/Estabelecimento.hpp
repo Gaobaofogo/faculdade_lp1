61d4d65d98b111831d3e67b838e2765c5cc08724
Gabriel <gabrieldiniz54@gmail.com>
2020-07-10 20:39:22 -0300

    Lendo de arquivo e imprimindo produtos

--- atividade_04/include/Estabelecimento.hpp
+++ atividade_04/include/Estabelecimento.hpp
@@ -1,16 +1,20 @@
 #ifndef ESTABELECIMENTO_H
 #define ESTABELECIMENTO_H
 
+#include "Produto.hpp"
+
+#include <vector>
 #include <string>
 
 
 class Estabelecimento {
   public:
     std::string codigo;
-    std::string produto;
+    std::vector<Produto> produtos;
     std::string unidadeMedida;
     float preco;
 
+    Estabelecimento(const std::string& caminho_arquivo_estoque);
     void listar();
     void venda(const std::string& codigo);
     void caixa();
--- atividade_04/main.cpp
+++ atividade_04/main.cpp
@@ -1,19 +1,23 @@
 #include "Cliente.hpp"
+#include "Estabelecimento.hpp"
 
 #include <iostream>
 #include <vector>
 
 void print_client_menu(int qntdClientes);
 
+
 int main(int argc, char* argv[]) {
+  Estabelecimento estabelecimento("estoque.csv");
   std::vector<Cliente> clientes;
+  std::vector<Produtos> sacola;
   Cliente cliente_1(0);
   char opcao;
   bool continuar_no_programa = true;
 
   clientes.push_back(cliente_1);
 
-  std::cout << "Loja virtual 1.0" << std::endl << std::endl;
+  std::cout << "Loja virtual 1.0" << std::endl;
 
   while (continuar_no_programa == true) {
     print_client_menu(clientes.size());
@@ -29,7 +33,9 @@ int main(int argc, char* argv[]) {
 
       clientes[clientes.size() - 1].saldo = novo_saldo;
     } else if (opcao == '2') {
+      estabelecimento.listar();
     } else if (opcao == '3') {
+    } else if (opcao == '4') {
     } else if (opcao == '0') {
       char resposta;
 
@@ -41,6 +47,8 @@ int main(int argc, char* argv[]) {
       } else if (resposta == 'y' || resposta == 'Y') {
         Cliente novo_cliente(0);
         clientes.push_back(novo_cliente);
+      } else {
+        std::cout << "Opção não conhecida" << std::endl;
       }
     } else {
       std::cout << "Opção desconhecida pelo programa;" << std::endl;
@@ -51,11 +59,12 @@ int main(int argc, char* argv[]) {
 }
 
 void print_client_menu(int qntdClientes) {
-  std::cout << "Bem vindo a loja cliente " << qntdClientes << "." << std::endl;
-  std::cout << "Selecione uma opção:" << std::endl << std::endl;
+  std::cout << std::endl;
+  std::cout << "Selecione uma opção cliente " << qntdClientes << ":" << std::endl << std::endl;
   std::cout << "\t1) Modificar saldo" << std::endl;
   std::cout << "\t2) Ver produtos da loja" << std::endl;
   std::cout << "\t3) Ver conteúdo da sacola" << std::endl;
+  std::cout << "\t4) Por na sacola um item" << std::endl;
   std::cout << std::endl;
   std::cout << "\t0) Encerrar as atividades do cliente " << qntdClientes << std::endl;
 }
